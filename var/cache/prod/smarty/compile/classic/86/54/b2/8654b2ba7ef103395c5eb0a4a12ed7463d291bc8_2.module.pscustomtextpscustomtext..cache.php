0f198552f405ac200633ea7af7eb0564dcaaabfd
aniabuto <aniabuto@wp.pl>
2021-11-13 17:03:29 +0100

    update

---
+++ var/cache/prod/smarty/compile/classic/86/54/b2/8654b2ba7ef103395c5eb0a4a12ed7463d291bc8_2.module.pscustomtextpscustomtext..cache.php
@@ -0,0 +1,32 @@
+<?php
+/* Smarty version 3.1.39, created on 2021-11-08 15:31:02
+  from 'module:pscustomtextpscustomtext.' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.39',
+  'unifunc' => 'content_618934a61001a2_98130141',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '8654b2ba7ef103395c5eb0a4a12ed7463d291bc8' => 
+    array (
+      0 => 'module:pscustomtextpscustomtext.',
+      1 => 1633801230,
+      2 => 'module',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_618934a61001a2_98130141 (Smarty_Internal_Template $_smarty_tpl) {
+$_smarty_tpl->compiled->nocache_hash = '1616976885618934a60feeb2_05225582';
+?>
+
+<div id="custom-text">
+  <?php echo $_smarty_tpl->tpl_vars['cms_infos']->value['text'];?>
+
+</div>
+<?php }
+}
