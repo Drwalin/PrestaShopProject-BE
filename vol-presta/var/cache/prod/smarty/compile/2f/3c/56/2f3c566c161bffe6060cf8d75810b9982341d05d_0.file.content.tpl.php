b46ecbe2fd884b6845c8ce1dfbde30866c1bbe6f
aniabuto <aniabuto@wp.pl>
2021-11-17 22:29:25 +0100

    deleted payment by check

--- vol-presta/var/cache/prod/smarty/compile/2f/3c/56/2f3c566c161bffe6060cf8d75810b9982341d05d_0.file.content.tpl.php
+++ vol-presta/var/cache/prod/smarty/compile/2f/3c/56/2f3c566c161bffe6060cf8d75810b9982341d05d_0.file.content.tpl.php
@@ -1,33 +1,3 @@
-<?php
-/* Smarty version 3.1.39, created on 2021-11-17 17:28:13
-  from '/var/www/html/admin1437/themes/new-theme/template/content.tpl' */
-
-/* @var Smarty_Internal_Template $_smarty_tpl */
-if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
-  'version' => '3.1.39',
-  'unifunc' => 'content_61952d9db9d926_27094062',
-  'has_nocache_code' => false,
-  'file_dependency' => 
-  array (
-    '2f3c566c161bffe6060cf8d75810b9982341d05d' => 
-    array (
-      0 => '/var/www/html/admin1437/themes/new-theme/template/content.tpl',
-      1 => 1633363913,
-      2 => 'file',
-    ),
-  ),
-  'includes' => 
-  array (
-  ),
-),false)) {
-function content_61952d9db9d926_27094062 (Smarty_Internal_Template $_smarty_tpl) {
-?>
-<div id="ajax_confirmation" class="alert alert-success" style="display: none;"></div>
-
-
-<?php if ((isset($_smarty_tpl->tpl_vars['content']->value))) {?>
-  <?php echo $_smarty_tpl->tpl_vars['content']->value;?>
-
-<?php }
-}
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:9aa79ecbfb5e7cf7686c518f1037afbb84be85aa4ee6de63c39a8bc2f930d716
+size 931
--- vol-presta/var/cache/prod/smarty/compile/classic/d8/78/01/d8780131386a9358b97ce5d4080996e8168525d3_0.file.popup.tpl.php
+++ vol-presta/var/cache/prod/smarty/compile/classic/d8/78/01/d8780131386a9358b97ce5d4080996e8168525d3_0.file.popup.tpl.php
@@ -1,29 +1,3 @@
-<?php
-/* Smarty version 3.1.39, created on 2021-11-17 17:28:13
-  from '/var/www/html/modules/welcome/views/templates/popup.tpl' */
-
-/* @var Smarty_Internal_Template $_smarty_tpl */
-if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
-  'version' => '3.1.39',
-  'unifunc' => 'content_61952d9dc22548_80247173',
-  'has_nocache_code' => false,
-  'file_dependency' => 
-  array (
-    'd8780131386a9358b97ce5d4080996e8168525d3' => 
-    array (
-      0 => '/var/www/html/modules/welcome/views/templates/popup.tpl',
-      1 => 1624549278,
-      2 => 'file',
-    ),
-  ),
-  'includes' => 
-  array (
-  ),
-),false)) {
-function content_61952d9dc22548_80247173 (Smarty_Internal_Template $_smarty_tpl) {
-?>
-<div class="onboarding-popup bootstrap">
-  <div class="content"></div>
-</div>
-<?php }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:f874629e81e7e4fc3f81d4452e3a63b71964f57746e9b9db5826ced63a85fc79
+size 784
