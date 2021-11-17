5604a504b53dcbd7dbef034dd6147fec69eba43c
311Volt <113amper@gmail.com>
2021-11-17 17:56:29 +0100

    add volume directories for containers

---
+++ vol-presta/var/cache/prod/smarty/compile/classic/30/0f/0e/300f0e3cd4f5d67dc83431d2c105c0a681c82d2f_0.file.navbar.tpl.php
@@ -0,0 +1,48 @@
+<?php
+/* Smarty version 3.1.39, created on 2021-11-17 17:27:45
+  from '/var/www/html/modules/welcome/views/navbar.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.39',
+  'unifunc' => 'content_61952d81814bf4_87673261',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '300f0e3cd4f5d67dc83431d2c105c0a681c82d2f' => 
+    array (
+      0 => '/var/www/html/modules/welcome/views/navbar.tpl',
+      1 => 1624549278,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_61952d81814bf4_87673261 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+<div class="onboarding-navbar bootstrap">
+  <div class="row text">
+    <div class="col-md-8">
+      <?php echo call_user_func_array( $_smarty_tpl->smarty->registered_plugins[Smarty::PLUGIN_FUNCTION]['l'][0], array( array('s'=>'Launch your shop!','d'=>'Modules.Welcome.Admin'),$_smarty_tpl ) );?>
+
+    </div>
+    <div class="col-md-4 text-right text-md-right"><?php echo $_smarty_tpl->tpl_vars['percent_rounded']->value;?>
+%</div>
+  </div>
+  <div class="progress">
+    <div class="bar" role="progressbar" style="width:<?php echo $_smarty_tpl->tpl_vars['percent_real']->value;?>
+%;"></div>
+  </div>
+  <div>
+    <button class="btn btn-main btn-sm onboarding-button-resume"><?php echo call_user_func_array( $_smarty_tpl->smarty->registered_plugins[Smarty::PLUGIN_FUNCTION]['l'][0], array( array('s'=>'Resume','d'=>'Modules.Welcome.Admin'),$_smarty_tpl ) );?>
+</button>
+  </div>
+  <div>
+    <a class="btn -small btn-main btn-sm onboarding-button-stop"><?php echo call_user_func_array( $_smarty_tpl->smarty->registered_plugins[Smarty::PLUGIN_FUNCTION]['l'][0], array( array('s'=>'Stop the OnBoarding','d'=>'Modules.Welcome.Admin'),$_smarty_tpl ) );?>
+</a>
+  </div>
+</div>
+<?php }
+}
