bea63d9d1d7fd18adbde9a998dbb91bb850cf040
311Volt <113amper@gmail.com>
2021-11-24 14:33:23 +0100

    rework scraper and add an import script for products

--- util-importer/importer.py
+++ util-importer/importer.py
@@ -1,6 +1,178 @@
 from prestapyt import PrestaShopWebServiceDict
+import requests
+import json
+import re
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+SCRAPER_OUTPUT_PATH = "../util-scraper/output/"
 WEBSERVICE_KEY = "9KLBU8X113FV5RTDZF42RVPX9EFVUQ6M"
-presta = PrestaShopWebServiceDict('https://localhost:80/api', WEBSERVICE_KEY)
 
-print(presta.get("products"))
+
+class ProgressCounter:
+    def __init__(self, numTasks):
+        self.counter = 0
+        self.numTasks = numTasks
+        self.progressStr = ""
+
+    def print_progress(self):
+        print("\b" * len(self.progressStr), end='', flush=True)
+        self.progressStr = "(%d/%d)" % (self.counter, self.numTasks)
+        print(self.progressStr, end='', flush=True)
+        self.counter += 1
+
+    def end(self):
+        self.print_progress()
+        print("")
+
+def read_json(filename):
+	ret = None
+	with open(filename, "r") as jsonfile:
+		ret = json.load(jsonfile)
+	return ret
+
+def langstr(langid, value):
+	return {"attrs":{"id":langid}, "value": value}
+
+def make_url_friendly(string):
+	ret = re.sub(r"\s+", '-', string)
+	ret = re.sub("[^0-9a-zA-Z]+", "-", ret)
+	return ret.lower()
+
+
+def create_category_dict(category, category_map):
+	template = read_json("template-category.json")
+
+	template["category"]["name"]["language"] = langstr("2",category["name"])
+	template["category"]["link_rewrite"]["language"] = langstr("2", make_url_friendly(category["name"]))
+	# template["category"]["id"] = category["id"]
+	if "parent_id" in category:
+		template["category"]["id_parent"] = category_map[category["parent_id"]]
+	else:
+		template["category"]["id_parent"] = "2"
+
+	return template
+
+def create_product_dict(product, category_map):
+	template = read_json("template-product.json")
+
+	categories = [{"id": "2"}] + [{"id": category_map[cd["id"]]} for cd in product["categories"]]
+
+
+	template["product"]["name"]["language"] = langstr("2",product["name"])
+	template["product"]["description"]["language"] = langstr("2",product["desc"])
+	template["product"]["description_short"]["language"] = langstr("2",product["short_desc"])
+	template["product"]["id_category_default"] = category_map[product["main_category"]]
+	template["product"]["price"] = product["price"]
+	template["product"]["associations"]["categories"]["category"] = categories #product["categories"]
+
+	return template
+
+def create_combination_dict(prod_id, option_value_id):
+	template = read_json("template-combination.json")
+
+	template["combination"]["id_product"] = prod_id
+	template["combination"]["associations"]["product_option_values"]["product_option_value"]["id"] = str(option_value_id)
+
+	return template
+
+def create_option_value_dict(name, position):
+	template = read_json("template-product-option-value.json")
+
+	template["product_option_value"]["position"] = str(position)
+	template["product_option_value"]["name"]["language"] = langstr("2",name)
+
+	return template
+
+
+def upload_image(pws, resourceurl, imgfilename):
+	with open(imgfilename, "rb") as img:
+		pws.add(resourceurl, files=[("image", imgfilename, img.read())])
+	pass
+
+def upload_category(pws, category, category_map):
+	return pws.add("categories", create_category_dict(category, category_map))
+
+def upload_combination(pws, prod_id, prod_option_value):
+	return pws.add("combinations", create_combination_dict(prod_id, prod_option_value))
+
+product_option_values = ["26", "27", "28"]
+
+def upload_product(pws, product, category_map):
+	new_prod = presta.add("products", create_product_dict(product, category_map))
+	new_prod_id = new_prod["prestashop"]["product"]["id"]
+
+	for val in product_option_values:
+		upload_combination(pws, new_prod_id, val)
+
+	upload_image(presta, "images/products/"+new_prod_id, SCRAPER_OUTPUT_PATH + product["img"])
+
+
+def get_list_of_ids(pws, table_name, table_name_singular):
+	li = pws.get(table_name)
+	if len(li[table_name]) == 0:
+		return []
+	li1 = li[table_name][table_name_singular]
+	#print(json.dumps(li))
+	#print(li1)
+	if isinstance(li1, list):
+		return [r["attrs"]["id"] for r in li[table_name][table_name_singular]]
+	return [li1["attrs"]["id"]]
+
+def purge_products(pws):
+	ids = get_list_of_ids(pws, "products", "product")
+	if len(ids):
+		pws.delete("products", resource_ids=ids)
+
+def purge_categories(pws):
+	ids = get_list_of_ids(pws, "categories", "category")
+	ids = [i for i in ids if int(i)>=3]
+	if len(ids):
+		pws.delete("categories", resource_ids=ids)
+
+ses = requests.Session()
+ses.verify = False
+presta = PrestaShopWebServiceDict('https://localhost:443/api', WEBSERVICE_KEY, session=ses)
+#presta.debug = True
+
+
+presta.get("products")
+
+print("Purging products...")
+purge_products(presta)
+
+print("Purging categories...")
+purge_categories(presta)
+#exit()
+
+#print(presta.add("products", read_json("template-product.json")))
+#exit()
+p_category_map = dict()
+s_products = read_json(SCRAPER_OUTPUT_PATH + "items.json")
+s_categories = read_json(SCRAPER_OUTPUT_PATH + "categories.json")
+
+print("Uploading categories...")
+progCounter = ProgressCounter(len(s_categories))
+for category in s_categories:
+	progCounter.print_progress()
+	resp = upload_category(presta, category, p_category_map)
+	#print("XDDDDDDDDD")
+	#print(json.dumps(resp,indent=4))
+	#print("XDDDDDDDD")
+	p_category_map[category["id"]] = resp["prestashop"]["category"]["id"]
+progCounter.end()
+
+
+#print(json.dumps(presta.get("combinations/10"), indent=4))
+#exit()
+
+
+print("Uploading products...", end='')
+progCounter = ProgressCounter(len(s_products))
+for product in s_products:
+	progCounter.print_progress()
+	upload_product(presta, product, p_category_map)
+	#exit()
+progCounter.end()
+
+print("")
--- util-scraper/etrapez_scraper.py
+++ util-scraper/etrapez_scraper.py
@@ -9,6 +9,7 @@ import json
 import csv
 import time
 import sys
+import copy
 
 def url_cache_path(url):
     urlHash = hashlib.sha1(url.encode('utf-8')).hexdigest()
@@ -24,26 +25,20 @@ def get_resource_from_cache(url):
     return None
 
 def cache_webpage_content(url, content):
-    try:
-        os.mkdir("htmlcache")
-    except FileExistsError:
-        pass
-
     with gzip.open(url_cache_path(url), "w") as file:
-        file.write(content.encode('utf-8'))
+        file.write(content)
     pass        
 
 def get_resource(url):
     if is_url_cached(url):
         return get_resource_from_cache(url)
-
     response = urllib.request.urlopen(url)
-    content = response.read().decode('utf-8')
+    content = response.read()
     cache_webpage_content(url, content)
     return content
 
 def get_webpage_tree(url):
-    content = get_resource(url)
+    content = get_resource(url).decode('utf-8')
     return lh.fromstring(content)
 
 class ProgressCounter:
@@ -62,10 +57,35 @@ class ProgressCounter:
         self.print_progress()
         print("")
 
+def register_category(cat_dict,  cat_csv_dict, cat_name):
+    if cat_name in cat_dict:
+        return cat_dict[cat_name]
 
+    cat_id = str(len(cat_dict)+3)
+    cat_dict[cat_name] = cat_id
+    csv_entry = {"id": cat_id, "name": cat_name}
+    if i > 1:
+        csv_entry["parent_id"] = str(cat_dict[catNodes[i-1].text])
+    cat_csv_dict.append(csv_entry)
+    return cat_id
+
+def get_html_node_source(node, ignore_elements=[]):
+    ret = ""
+    node1 = copy.deepcopy(node)
+    for elem_name in ignore_elements:
+        for r_node in node1.xpath(".//"+elem_name):
+            r_node.find("..").remove(r_node)
+    for child in node1:
+        ret += lh.tostring(child, encoding="unicode")
+    return ret
 
 
 MAIN_URL = "https://etrapez.pl/kursy"
+
+os.makedirs("httpcache", exist_ok=True)
+os.makedirs("output", exist_ok=True)
+os.makedirs("output/images", exist_ok=True)
+
 
 print("Fetching " + MAIN_URL + "...")
 tree0 = get_webpage_tree(MAIN_URL)
@@ -96,40 +116,47 @@ for url in productURLs:
     pageTree = get_webpage_tree(url)
 
     product = dict()
-    product["Enabled"] = 1
 
     titleNode = pageTree.xpath("//h1[contains(@class,'product_title entry-title')]")
-    product["Name"] = titleNode[0].text
+    product["name"] = titleNode[0].text
 
-    descNodes = pageTree.xpath("//div[contains(@class,'woocommerce-product-details__short-description')]/descendant-or-self::*/text()")
-    product["Short desc."] = ''.join(descNodes).replace(u"\u00a0", " ")
+    shortDescNode = pageTree.xpath("//div[contains(@class,'woocommerce-product-details__short-description')]")
+    product["short_desc"] = get_html_node_source(shortDescNode[0], ignore_elements=["img", "iframe"])
+
+    longDescNode = pageTree.xpath("//div[contains(@id,'tab-description')]")
+    product["desc"] = get_html_node_source(longDescNode[0], ignore_elements=["img", "iframe", "p[contains(@class,'product')]"])
+
     catNodes = pageTree.xpath("//nav[contains(@class, 'woocommerce-breadcrumb')]//a")
-    catstr = ""
+    prodCategories = []
     for i in range(1, len(catNodes)):
-        catName = catNodes[i].text
-        if catName not in categories:
-            catId = str(len(categories)+3)
-            categories[catName] = catId
-            catCSVDict = {"ID": catId, "Name": catName}
-            if i > 1:
-                catCSVDict["ParentID"] = str(categories[catNodes[i-1].text])
-            categoriescsv.append(catCSVDict)
+        cid = register_category(categories, categoriescsv, catNodes[i].text)
+        if cid:
+            prodCategories.append(cid)
 
-        catstr += categories[catName]
-        if i < len(catNodes)-1:
-            catstr += ","
-    product["Categories"] = catstr
-    #product["Categories"] = catNodes[1].text + ", " + catNodes[2].text
+    product["categories"] = [{"id": cid} for cid in prodCategories]
+    if len(prodCategories):
+        product["main_category"] = prodCategories[-1]
+
 
     priceNode = pageTree.xpath("//div[contains(@class,'summary')]//bdi")
-    product["Price"] = priceNode[0].text[:-1] + " PLN"
+    product["price"] = priceNode[0].text[:-1].replace(",",".")
+
+
+
 
     imgNode = pageTree.xpath("//div[contains(@class,'feat_image')]/a[1]")
-    product["Images URL"] = imgNode[0].get("href")
+    imgURL = imgNode[0].get("href")
+    imgOutFileName = "images/" + hashlib.sha1(imgURL.encode('utf-8')).hexdigest() + "." + imgURL.split('.')[-1]
+
+    imgFileData = get_resource(imgURL)
+    with open("output/"+imgOutFileName, "wb") as imgOutFile:
+        imgOutFile.write(imgFileData)
+    product["img"] = imgOutFileName
 
     fetchResult.append(product)
 progCounter.end()
 
+'''
 with open("output/items.csv", "w") as fcsv:
     fieldnames = fetchResult[0].keys()
     writer = csv.DictWriter(fcsv, delimiter=';', fieldnames=fieldnames)
@@ -143,10 +170,14 @@ with open("output/categories.csv", "w") as fcsv:
 
     writer.writeheader()
     writer.writerows(categoriescsv)
+'''
 
-'''
 print("Saving JSON...")
-with open("shop.csv", "w") as csvFile:
-    outFile.write(json.dumps(fetchResult, indent=4))
-'''
+
+with open("output/categories.json", "w") as jsonFile:
+    jsonFile.write(json.dumps(categoriescsv, indent=4))
+
+with open("output/items.json", "w") as jsonFile:
+    jsonFile.write(json.dumps(fetchResult, indent=4))
+
 print("done")
